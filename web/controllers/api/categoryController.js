f932403755bda32563d2f45090d28b90bc83981b
gguo <gguo@movoto.com>
2017-12-11 16:20:47 -0800

    fixed init error

--- web/controllers/api/categoryController.js
+++ web/controllers/api/categoryController.js
@@ -3,30 +3,32 @@ const CategoryFactory = require('../../fatories/categories/categoryFactory');
 class apiCategoryController extends BaseController {
     constructor() {
         super();
-        this.categoryFactory = new CategoryFactory();
     }
 
     async getCategories(req, res, next) {
         this.initialize(req, res, next);
-        
+        this.categoryFactory = new CategoryFactory();
         const categories = await this.categoryFactory.getCategories(req.query);
         this.sendJson(categories);
     }
 
     async createCategory(req, res, next) {
         this.initialize(req, res, next);
+        this.categoryFactory = new CategoryFactory();
         const category = await this.categoryFactory.createCategory(req.body);
         this.sendJson(category);
     }
 
     async updateCategory(req, res, next) {
         this.initialize(req, res, next);
+        this.categoryFactory = new CategoryFactory();
         const category = await this.categoryFactory.updateCategory(req.body);
         this.sendJson(category);
     }
 
     async deleteCategory(req, res, next) {
         this.initialize(req, res, next);
+        this.categoryFactory = new CategoryFactory();
         const category = await this.categoryFactory.deleteCategory(req.body);
         this.sendJson(category);
     }
