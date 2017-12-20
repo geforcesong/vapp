3be633941cf362fce474cd56333a80d0491c0f42
gguo <gguo@movoto.com>
2017-12-20 10:55:42 -0800

    added create product

---
+++ web/views/admin/client/code/features/products/createProduct.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+    el-container
+        el-header
+                h1 Create new product
+        el-main
+            el-row
+                el-col(:span="24")
+                    textIo(:styleObject="{'height':'600px'}" initText="abcdefg")
+</template>
+<script>
+import textIo from "../../common/components/editor";
+export default {
+  components: {
+    textIo
+  }
+}
+</script>
