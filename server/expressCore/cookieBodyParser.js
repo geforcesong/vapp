4f8774c9840151d1d1a5eba0dd8b61935cb52e01
gguo <gguo@movoto.com>
2017-12-21 15:31:43 -0800

    added limitation for 50mb

--- server/expressCore/cookieBodyParser.js
+++ server/expressCore/cookieBodyParser.js
@@ -2,6 +2,6 @@ var cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 module.exports = function (app) {
     app.use(cookieParser());
-    app.use(bodyParser.json());
+    app.use(bodyParser.json({ limit: '50mb', type: 'application/json' }));
     app.use(bodyParser.urlencoded({ extended: false }));
 }
